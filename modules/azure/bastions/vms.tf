f3bdc4e354a02b9869485fc7b93fbf45c8b6c551
Kevin Sandermann <kevin.sandermann@gmail.com>
2019-07-25 11:03:07 +0200

    implemented av-sets for bastions

--- modules/azure/bastions/vms.tf
+++ modules/azure/bastions/vms.tf
@@ -1,3 +1,15 @@
+resource "azurerm_availability_set" "bastions" {
+  name                          = "${var.bastions_vm_prefix}s.${var.cluster_fqdn}"
+  location                      = var.cluster_location
+  resource_group_name           = var.out_platform_rg_name
+  managed                       = true
+  platform_fault_domain_count   = var.bastions_fault_domains
+  platform_update_domain_count  = (var.bastions_fault_domains > 1)  ? 3 : 2
+  tags                          = var.cluster_resource_tags
+
+}
+
+
 resource "azurerm_virtual_machine" "bastions" {
   count                             = var.bastions_amount
   name                              = "${var.bastions_vm_prefix}${count.index +1}.${var.cluster_fqdn}"
@@ -6,11 +18,10 @@ resource "azurerm_virtual_machine" "bastions" {
   network_interface_ids             = [var.out_bastions_subnet_nics_ids[count.index]]
   primary_network_interface_id      =  var.out_bastions_subnet_nics_ids[count.index]
   vm_size                           = var.bastions_vm_type
-  //use modulo 3 as only regions with 3 av. zones are supported
-  zones                             = [(count.index%3)+1]
   tags                              = var.cluster_resource_tags
   delete_os_disk_on_termination     = true
   delete_data_disks_on_termination  = true
+  availability_set_id               = azurerm_availability_set.bastions.id
 
   //LRS for OS disks is sufficient as bastion functionality is not crucial for cluster operations
   storage_image_reference {
@@ -43,4 +54,12 @@ resource "azurerm_virtual_machine" "bastions" {
       path                          = "/home/${var.bastions_admin_username}/.ssh/authorized_keys"
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      "storage_os_disk",
+      "storage_data_disk",
+    ]
+  }
+
 }
--- providers/azure/vars.tf
+++ providers/azure/vars.tf
@@ -85,6 +85,12 @@ variable "bastions_amount" {
   default = "2"
 }
 
+variable "bastions_fault_domains" {
+  type = "string"
+  description = "Number of fault domains for the availibility set of the bastions. This is depending on the region that you are using. https://github.com/MicrosoftDocs/azure-docs/blob/master/includes/managed-disks-common-fault-domain-region-list.md"
+  default = "2"
+}
+
 variable "bastions_vm_type" {
   type = "string"
   description = "Type of the vm for the bastion. Example: Standard_DS1_v2"
