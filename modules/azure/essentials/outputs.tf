99656829d784d96e8e72645f78305d7bdaa777b3
Kevin Sandermann <kevin.sandermann@gmail.com>
2019-07-25 14:10:16 +0200

    destroy now keeping the dns zone

--- modules/azure/essentials/outputs.tf
+++ modules/azure/essentials/outputs.tf
@@ -6,6 +6,6 @@ output "out_platform_rg_name" {
 
 // need to output this to ensure dns zone exists when applying other resources on first bootstrap
 output "out_platform_dns_zone_name" {
-  value       = azurerm_dns_zone.cluster.name
+  value       = azurerm_dns_zone.platform.name
   description = "Name of the dns zone that was created."
 }
