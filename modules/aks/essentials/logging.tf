a0c91c2b4ceeda9ca00ac4274debd3c10d728b2e
Semjon Bibow <5166002+sbibow@users.noreply.github.com>
2019-08-09 13:13:18 +0200

    Fix logging replace logic (#20)
    It has not been interpreted as regex, because it has not
    been wrapped in forward slashes.
    
    Ref:
    - https://www.terraform.io/docs/configuration/functions/replace.html

--- modules/aks/essentials/logging.tf
+++ modules/aks/essentials/logging.tf
@@ -3,7 +3,7 @@ resource "random_id" "log_analytics_workspace_name_suffix" {
 }
 
 resource "azurerm_log_analytics_workspace" "platform" {
-  name                  = "${replace(var.platform_fqdn, "[^a-zA-Z0-9\\-]", "-")}-${random_id.log_analytics_workspace_name_suffix.dec}"
+  name                  = "${replace(var.platform_fqdn, "/[^a-zA-Z0-9\\-]/", "-")}-${random_id.log_analytics_workspace_name_suffix.dec}"
   location              = azurerm_resource_group.platform.location
   resource_group_name   = azurerm_resource_group.platform.name
   sku                   = "PerGB2018"
