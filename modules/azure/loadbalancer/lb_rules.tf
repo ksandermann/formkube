6f6d4118f8cfb8473e18a073da8b953dc894cca9
ksandermann <kevin.sandermann@gmail.com>
2019-09-18 14:54:20 +0200

    implemented probe_id for lb rules

--- modules/azure/loadbalancer/lb_rules.tf
+++ modules/azure/loadbalancer/lb_rules.tf
@@ -7,6 +7,7 @@ resource "azurerm_lb_rule" "ingresscontroller" {
   backend_port                   = var.loadbalancer_ingresscontroller_backend_port
   frontend_ip_configuration_name = azurerm_lb.loadbalancer.frontend_ip_configuration.0.name
   backend_address_pool_id        = azurerm_lb_backend_address_pool.nodes.id
+  //probe_id                        = "${azurerm_lb.loadbalancer.id}/probes/${azurerm_lb_probe.ingresscontroller.name}"
   // KNOWN ISSUE - see README.MD
   // probe_id                    = "${azurerm_lb_probe.ingress_controller.id}}"
   idle_timeout_in_minutes        = 5
@@ -23,6 +24,7 @@ resource "azurerm_lb_rule" "k8s_api" {
   backend_port                   = var.loadbalancer_k8s_api_backend_port
   frontend_ip_configuration_name = azurerm_lb.loadbalancer.frontend_ip_configuration.0.name
   backend_address_pool_id        = azurerm_lb_backend_address_pool.masters.id
+  //probe_id                        = "${azurerm_lb.loadbalancer.id}/probes/${azurerm_lb_probe.k8s_api.name}"
   // KNOWN ISSUE - see README.MD
   // probe_id                    = "${azurerm_lb_probe.ingress_controller.id}}"
   idle_timeout_in_minutes        = 5
@@ -40,6 +42,7 @@ resource "azurerm_lb_rule" "additional_ports" {
   backend_port                   = local.additional_node_rules_backend_ports[count.index]
   frontend_ip_configuration_name = azurerm_lb.loadbalancer.frontend_ip_configuration.0.name
   backend_address_pool_id        = azurerm_lb_backend_address_pool.nodes.id
+  //probe_id                        = "${azurerm_lb.loadbalancer.id}/probes/${azurerm_lb_probe.additional_node_probes.*.name[count.index]}"
   // KNOWN ISSUE - see README.MD
   // probe_id                    = "${azurerm_lb_probe.ingress_controller.id}}"
   idle_timeout_in_minutes        = 5
